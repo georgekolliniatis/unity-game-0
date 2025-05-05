b93f99c5d02fe99f531e11b91569f642a841ec12
George Kolliniatis <47611955+georgekolliniatis@users.noreply.github.com>
2025-05-05 21:09:47 +0300

    Define IInteractor interface

--- Assets/Game/Data/Interactions/IInteractor.cs
+++ Assets/Game/Data/Interactions/IInteractor.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IInteractor : MonoBehaviour
+namespace Assets.Game.Data.Interactions
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IInteractor
     {
-        
-    }
+        void OnStopInteraction();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        void OnUnfocus();
+
+        void OnFocus(IInteractable _);
+
+        void OnStartInteraction();
+
+        void OnCheckForInteractable();
     }
 }
